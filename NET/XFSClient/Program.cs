4efbb7a2c39935ff4cfd4a5b1eac3a05fa7c0385
Timofey Chudov <tchoudov@gmail.com>
2023-04-07 21:01:48 +0300

    Add try catch

--- NET/XFSClient/Program.cs
+++ NET/XFSClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Grpc.Net.Client;
 using Xfs;
+using Grpc.Core;
 
 var httpHandler = new HttpClientHandler();
 // Return `true` to allow certificates that are untrusted/invalid
@@ -12,7 +13,15 @@ using var channel = GrpcChannel.ForAddress("http://localhost:5214",
 
 //using var channel = GrpcChannel.ForAddress("https://localhost:7061");
 var client = new Xfs.XFSEmulatorSPI.XFSEmulatorSPIClient(channel);
-var reply = await client.WFSEventAsync(new Xfs.WFSEventRequest() { EventId = 1, ServiceName = "SIU" } );
-Console.WriteLine("XFS event sent");
+try
+{
+    var reply = await client.WFSEventAsync(new Xfs.WFSEventRequest() { EventId = 1, ServiceName = "SIU" } );
+    Console.WriteLine("XFS event sent");
+}
+catch (RpcException ex) 
+{
+    Console.WriteLine($"Failed to send XFS event - {ex}");
+}
+
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
